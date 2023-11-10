c667cb2ac76c52aef55db3cf25626b27a4e9111b
bai <965869256@qq.com>
2023-11-10 19:12:34 +0800

    add

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace AutoMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(43, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSalmon;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(173, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(180, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "5";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(43, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 29);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "默认间隔(5s) ";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.Location = new System.Drawing.Point(43, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(99, 29);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = " 滚动总次数  ";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox4.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(180, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(75, 26);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "次数";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton1.Location = new System.Drawing.Point(161, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(50, 21);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "粘贴";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton2.Location = new System.Drawing.Point(43, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 21);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "↓滚动";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton3.Location = new System.Drawing.Point(99, 6);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 21);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "↑滚动";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(299, 166);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "自动滚轮";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AutoMouse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //选择：滚动或者粘贴
+        private static Boolean boolGun=false;
+        private static Boolean boolZhantie=false;
+        private static Boolean boolGunShang = false;
+        
+        private static int count = -100;//默认下滚动
+        private int state = -1;//上下
+        private int time = 5000;
+        private int timeNums = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int mouse_event(int mouseevent, int dx, int dy, int cButtons, int dwExtraInfo);
+        [System.Runtime.InteropServices.DllImport("User32")]
+        public extern static void SetCursorPos(int x, int y);
+        const int MOUSEEVENTF_MOVE = 0x0001;            //移动鼠标 
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;        //模拟鼠标左键按下 
+        const int MOUSEEVENTF_LEFTUP = 0x0004;          //模拟鼠标左键抬起 
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;       //模拟鼠标右键按下 
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;         //模拟鼠标右键抬起 
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;      //模拟鼠标中键按下 
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;        //模拟鼠标中键抬起 
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;        //标示是否采用绝对坐标 
+        const int MOUSEEVENTF_WHEEL = 0x800;            //模拟鼠标滑轮移动
+
+        //开始
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (false == boolGun && false == boolZhantie && false == boolGunShang)
+            {
+                MessageBox.Show("请选择操作类型？ ", "提示!", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            }
+                       
+            timer1.Enabled = true;
+            try {                
+                //获取时间间隔 
+                timer1.Interval = 1000*int.Parse(double.Parse(textBox1.Text).ToString());
+            } catch {
+                //默认时间
+                timer1.Interval = 5000;
+            }
+        }
+
+        //停止
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (false == boolGun && false == boolZhantie && false == boolGunShang)
+            {
+                MessageBox.Show("请选择操作类型~", "警告!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            timer1.Enabled = false;
+           // textBox4.Text = "次数";
+            //timer1.Enabled = false;
+        } 
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //1 开始滚动
+            if (false == boolGun && false == boolZhantie && false == boolGunShang) return;
+            if (true == boolGun)//像下滚
+            {
+                if (state == 1)
+                {
+                    count = 100;//
+                }
+                else
+                {
+                    count = -100;//负数代表向下移动
+                }
+
+                //滚动鼠标一次
+                gunDong();
+            }
+            //1.1 向上滚
+            if (true == boolGunShang)
+            {
+                if (state == 1)
+                {
+                    count = -100;//
+                }
+                else
+                {
+                    count = 100;//正数代表向上移动
+                }
+                //滚动鼠标一次
+                gunDong();
+            }
+            //2 粘贴 并回车
+            if (true == boolZhantie) {
+                // 实例化一个 KeyEventArgs 对象
+                /*KeyEventArgs keyControl = new KeyEventArgs(Keys.Control);
+                KeyCVDown(sender,keyControl);*/
+                KeyCVDown2(sender,e, timeNums);
+
+                //this.KeyDown += new System.Windows.Forms.KeyEventHandler(KeyCVDown);
+            }
+         //计数
+            timeNums++;
+            textBox4.Text = "" + timeNums;
+
+       /*     if (timer1.Enabled ==true) {
+                timeNums++;
+                textBox4.Text = "" + timeNums;
+            } */         
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //鼠标滚动
+        private void gunDong() {
+            try
+            {
+                mouse_event(MOUSEEVENTF_WHEEL, 0, 0, count, 0);   //控制鼠标滑轮滚动，count代表滚动的值，负数代表向下，正数代表向上，如-100代表向下滚动100的y坐标
+            }
+            catch (Exception eee)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("引发此异常的原因是您输入的滚轮数值为非法格式\n您可以单击“确定”并检查输入信息后重试\n详细错误信息为：" + eee.Message, "错误提示", MessageBoxButtons.OK);
+            }
+        }
+
+        //1 执行粘贴和回车
+        private void KeyCVDown(object sender, KeyEventArgs e)
+        {
+            //if (e.Control && e.KeyCode == Keys.V)
+            if (e.Control)
+            {
+                // 执行粘贴操作
+                Clipboard.SetText("test11");
+                SendKeys.SendWait("^v"); // 发送Ctrl+V组合键
+                SendKeys.SendWait("{ENTER}"); // 发送回车键                
+            }
+        }
+        //2 执行粘贴和回车
+        private void KeyCVDown2(object sender, EventArgs e,int num)
+        {            
+            // 获取剪贴板内容
+            string clipboardText = Clipboard.GetText();
+            if ("" == clipboardText|| null == clipboardText) clipboardText = "剪切板没有内容!";
+
+            Clipboard.SetText(clipboardText+ num);
+            SendKeys.SendWait("^v"); // 发送Ctrl+V组合键
+            Clipboard.SetText(clipboardText );
+            // 将剪贴板内容插入到文本框中
+            //textBox1.Text = clipboardText;
+            // 模拟按下回车键
+            SendKeys.SendWait("{ENTER}");
+        }
+
+        //滚动选中 下滚
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            boolGun = radioButton2.Checked;
+        }
+        //向上滚
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            boolGunShang = radioButton3.Checked;
+        }
+
+        // 粘贴是否选中
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            boolZhantie=radioButton1.Checked;
+        }
+        private void tanhchuang(object sender, EventArgs e)
+        {
+            MessageBox.Show("这是一个弹窗", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
